f070d1ccb65c940a3574a459d310f4bf27b00300
brewinski <cbrewin93@gmail.com>
2024-09-18 10:09:19 +1000

    feat: bufferline, lazyvim and rust lsp enabled

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    vim.opt.termguicolors = true
+    require('bufferline').setup {
+      options = {
+        mode = 'buffers', -- set to "tabs" to only show tabpages instead
+        diagnostics = 'nvim_lsp',
+      },
+    }
+
+    vim.api.nvim_set_keymap('n', '<Tab>', '<CMD>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<S-Tab>', '<CMD>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap('n', '<S-t>', '<CMD>BufferLineCycleWindowlessToggle<CR>', { noremap = true, silent = true })
+  end,
+}
