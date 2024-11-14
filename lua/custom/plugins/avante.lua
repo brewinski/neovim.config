fc39fa1e69a8157ee2fef3cb9e6a12a66b545e1f
brewinski <cbrewin93@gmail.com>
2024-11-14 14:12:01 +1000

    feat (avante) avante with a custom cody provider and prototype context key

---
+++ lua/custom/plugins/avante.lua
@@ -0,0 +1,134 @@
+local avante_cody = require 'custom.scripts.avante-cody'
+
+return {
+  'yetone/avante.nvim',
+  -- name = 'yetone/avante.nvim',
+  -- dev = true,
+  -- dir = '~/Documents/github/avante.nvim',
+  event = 'VeryLazy',
+  lazy = true,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    -- add any opts here
+    provider = 'copilot',
+    auto_suggestions_provider = 'copilot',
+    copilot = {
+      endpoint = 'https://api.githubcopilot.com',
+      model = 'claude-3.5-sonnet',
+      proxy = nil, -- [protocol://]host[:port] Use this proxy
+      allow_insecure = false, -- Allow insecure server connections
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+    },
+    behaviour = {
+      auto_suggestions = true, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+    },
+    mappings = {
+      suggestion = {
+        accept = '<C-a>',
+      },
+    },
+    repo_map = {
+      ignore_patterns = { '%.git', '%.worktree', '__pycache__', 'node_modules', 'providers' }, -- ignore files matching these
+    },
+    vendors = {
+      ['work-cody'] = {
+        model = avante_cody.model,
+        endpoint = avante_cody.endpoint,
+        api_key_name = avante_cody.api_key_name,
+        max_tokens = avante_cody.max_tokens,
+        stream = false,
+        topK = avante_cody.topK,
+        topP = avante_cody.topP,
+        proxy = avante_cody.proxy,
+        allow_insecure = avante_cody.allow_insecure,
+        timeout = avante_cody.timeout,
+        temperature = avante_cody.temperature,
+        parse_curl_args = avante_cody.parse_curl_args,
+        parse_response_data = avante_cody.parse_response_data,
+      },
+      -- endpoint = 'https://sourcegraph.com',
+      -- model = 'anthropic::2024-10-22::claude-3-5-sonnet-latest',
+      -- timeout = 30000,
+      -- temperature = 0,
+      -- model = "anthropic::2024-10-22::claude-3-5-sonnet-latest"
+      -- model = "anthropic::2024-10-22::claude-3-5-haiku-latest"
+      -- model = "anthropic::2023-06-01::claude-3-haiku"
+      -- model = "mistral::v1::mixtral-8x7b-instruct"
+      -- model = "google::v1::gemini-1.5-flash"
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    {
+      'zbirenbaum/copilot.lua',
+      config = function()
+        require('copilot').setup {}
+      end,
+    }, -- for providers='copilot'
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+    {
+      'sourcegraph/sg.nvim',
+      config = function()
+        require('sg').setup {}
+      end,
+      dependencies = {
+        'nvim-lua/plenary.nvim', --[[ "nvim-telescope/telescope.nvim ]]
+      },
+      -- If you have a recent version of lazy.nvim, you don't need to add this!
+      build = 'nvim -l build/init.lua',
+    },
+  },
+}
+
+-- examples
+-- local rpc = require("sg.rpc")
+--
+-- local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+--
+-- vim.api.nvim_notify(path .. "\n\n\n\n\n\n\n\n\n\n", 1, {})
+--
+-- rpc.get_remote_url(
+--   path,
+--   function(err, remote_url) vim.api.nvim_notify("\nresults: " .. vim.inspect(remote_url), 1, {}) end
+-- )
+--
+-- rpc.get_search("AuthService", function(err, results)
+--   -- vim.api.nvim_notify("err: " .. vim.inspect(err) .. "\nresults: " .. vim.inspect(results), 1, {})
+-- end)
---
+++ lua/custom/scripts/avante-cody.lua
@@ -0,0 +1,292 @@
+--- Documentation for setting up Sourcegraph Cody
+--- Generating an access token: https://sourcegraph.com/docs/cli/how-tos/creating_an_access_token
+
+---@class AvanteProviderFunctor
+local M = {}
+
+M.endpoint = 'https://canstar.sourcegraphcloud.com'
+M.api_key_name = 'SRC_ACCESS_TOKEN'
+M.max_tokens = 4000
+M.stream = true
+M.topK = -1
+M.topP = -1
+M.model = 'anthropic/claude-3-5-sonnet-20240620'
+M.proxy = nil
+M.allow_insecure = false -- Allow insecure server connections
+M.timeout = 30000 -- Timeout in milliseconds
+M.temperature = 0
+
+M.cody_context = {}
+
+M.role_map = {
+  user = 'human',
+  assistant = 'assistant',
+  system = 'system',
+}
+
+M.parse_context_messages = function(context)
+  local codebase_context = {}
+
+  for _, blob in ipairs(context) do
+    local path = blob.blob.path
+    local file_content = blob.chunkContent
+
+    table.insert(codebase_context, {
+      speaker = M.role_map.user,
+      -- text = 'FILEPATH: ' .. path .. '\nCode:\n' .. file_content,
+      text = 'FILEPATH: ' .. vim.inspect(blob),
+    })
+    table.insert(codebase_context, {
+      speaker = M.role_map.assistant,
+      text = 'Ok.',
+    })
+  end
+
+  return codebase_context
+end
+
+M.parse_messages = function(opts)
+  local cody_system_prompt = {
+    -- '\n',
+    -- 'At the end of EVERY request, output a compact list of the files that were used to help generate the response.',
+    -- 'example:',
+    -- 'given these context files: CACTCHUP.md, pkg/gateway/gateway.go, pkg/gateway/request.go, pkg/gateway/response.go',
+    -- 'this will be the following output format:',
+    -- '## context used:',
+    -- '  - CACTCHUP.md:10-20',
+    -- '  - pkg/gateway/gateway.go:1-15',
+    -- '',
+    -- '## context files:',
+    -- '  - CACTCHUP.md:10-20',
+    -- '  - pkg/gateway/gateway.go:1-15',
+    -- '  - pkg/gateway/request.go:100-151',
+    -- '  - pkg/gateway/response.go:255-453',
+    -- '',
+    -- 'when working with markdown, please generate inner code block using ~~~ rather than ```. the output of this chat is rendered using markdown and so outputting ``` will mess with the renderer.',
+  }
+
+  local messages = {
+    { role = 'system', text = opts.system_prompt .. table.concat(cody_system_prompt, '\n') },
+  }
+
+  vim.iter(M.parse_context_messages(M.cody_context)):each(function(msg)
+    table.insert(messages, msg)
+  end)
+
+  vim.iter(opts.messages):each(function(msg)
+    table.insert(messages, { speaker = M.role_map[msg.role], text = msg.content })
+  end)
+
+  -- vim.api.nvim_notify(vim.inspect(messages), 1, {})
+
+  return messages
+end
+
+M.parse_response_data = function(data_stream, event_state, opts)
+  if event_state == 'done' then
+    opts.on_complete()
+    return
+  end
+
+  if data_stream == nil or data_stream == '' then
+    return
+  end
+
+  local json = vim.json.decode(data_stream)
+  local delta = json.deltaText
+  local stopReason = json.stopReason
+
+  if stopReason == 'end_turn' then
+    -- opts.on_chunk('\n\n## context files:\n  - ' .. table.concat(M.get_context_file_list(M.cody_context), '\n  - '))
+    return
+  end
+
+  opts.on_chunk(delta)
+end
+
+M.get_context_file_list = function(context)
+  local codebase_context = {}
+
+  for _, blob in ipairs(context) do
+    local path = blob.blob.path
+
+    table.insert(codebase_context, path)
+  end
+
+  return codebase_context
+end
+
+M.BASE_PROVIDER_KEYS = {
+  'endpoint',
+  'model',
+  'deployment',
+  'api_version',
+  'proxy',
+  'allow_insecure',
+  'api_key_name',
+  'timeout',
+  -- internal
+  'local',
+  '_shellenv',
+  'tokenizer_id',
+  'use_xml_format',
+  'role_map',
+}
+
+M.parse_config = function(opts)
+  local s1 = {}
+  local s2 = {}
+
+  for key, value in pairs(opts) do
+    if vim.tbl_contains(M.BASE_PROVIDER_KEYS, key) then
+      s1[key] = value
+    else
+      s2[key] = value
+    end
+  end
+
+  return s1,
+    vim
+      .iter(s2)
+      :filter(function(_, v)
+        return type(v) ~= 'function'
+      end)
+      :fold({}, function(acc, k, v)
+        acc[k] = v
+        return acc
+      end)
+end
+
+M.parse_curl_args = function(provider, code_opts)
+  local base, body_opts = M.parse_config(provider)
+
+  local api_key = provider.parse_api_key()
+  if api_key == nil then
+    -- if no api key is available, make a request with a empty api key.
+    api_key = ''
+  end
+
+  local headers = {
+    ['Content-Type'] = 'application/json',
+    ['Authorization'] = 'token ' .. api_key,
+  }
+
+  -- Get Cody context
+  -- local context_query = code_opts.messages[#code_opts.messages].content -- Use the last message as the context query
+  -- M.get_cody_context(base.endpoint, context_query, api_key)
+  vim.api.nvim_notify(api_key .. '\n\n\n\n\n\n', 1, {})
+
+  return {
+    url = base.endpoint .. '/.api/completions/stream?api-version=2&client-name=web&client-version=0.0.1',
+    timeout = base.timeout,
+    insecure = false,
+    headers = headers,
+    body = vim.tbl_deep_extend('force', {
+      model = base.model,
+      temperature = body_opts.temperature,
+      topK = body_opts.topK,
+      topP = body_opts.topP,
+      maxTokensToSample = body_opts.max_tokens,
+      stream = true,
+      messages = M.parse_messages(code_opts),
+    }, {}),
+  }
+end
+
+M.on_error = function() end
+
+M.get_cody_context = function(endpoint, query, api_key, repo)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+  local headers = {
+    ['Content-Type'] = 'application/json; charset=utf-8',
+    ['x-sourcegraph-client'] = endpoint,
+    ['Authorization'] = 'token ' .. api_key,
+  }
+
+  -- local current_repo = vim.fn.system('git rev-parse --show-toplevel'):gsub('\n', '')
+  -- current_repo = vim.fn.fnamemodify(current_repo, ':t')
+  local current_repo = 'UmVwb3NpdG9yeToxNjI='
+
+  local body = {
+    query = [[
+      query GetCodyContext($repos: [ID!]!, $query: String!, $codeResultsCount: Int!, $textResultsCount: Int!, $filePatterns: [String!]) {
+        getCodyContext(repos: $repos, query: $query, codeResultsCount: $codeResultsCount, textResultsCount: $textResultsCount, filePatterns: $filePatterns) {
+          ...on FileChunkContext {
+            blob {
+              path
+              repository {
+                id
+                name
+              }
+              commit {
+                oid
+              }
+              url
+            }
+            startLine
+            endLine
+            chunkContent
+            matchedRanges {
+              start {
+                line
+                column: character
+              }
+              end {
+                line
+                column: character
+              }
+            }
+          }
+        }
+      }
+    ]],
+    variables = {
+      repos = { repo or current_repo },
+      query = query,
+      codeResultsCount = 15,
+      textResultsCount = 5,
+    },
+  }
+
+  local callback = function(response)
+    -- Handle the response here
+    if response.status == 200 then
+      local data = vim.json.decode(response.body)
+
+      vim.api.nvim_notify(vim.inspect(response), 1, {})
+
+      if data.errors ~= nil then
+        print('Error: ' .. 'query errors.')
+        return nil
+      end
+
+      local context = data.data.getCodyContext
+      -- Process the data as needed
+
+      M.cody_context = context
+    else
+      print('Error: ' .. response.status)
+      return nil
+    end
+  end
+
+  if filetype == 'AvanteInput' then
+    -- vim.api.nvim_notify('async', 1, {})
+    local response = require('plenary.curl').post(endpoint .. '/.api/graphql?GetCodyContext', {
+      headers = headers,
+      body = vim.fn.json_encode(body),
+    })
+    callback(response)
+  else
+    -- vim.api.nvim_notify('sync', 1, {})
+    require('plenary.curl').post(endpoint .. '/.api/graphql?GetCodyContext', {
+      headers = headers,
+      body = vim.fn.json_encode(body),
+      callback = callback,
+    })
+  end
+end
+
+return M
